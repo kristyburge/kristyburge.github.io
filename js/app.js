1cb114a035d98e3cdd83d704ff8aae4f10571a87
Kristy Burge <hello@kristyburge.com>
2018-10-01 11:24:19 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).foundation();
+
+
+
+// Display the "to top" button only when the user has scrolled down the page 300px
+window.onscroll = function() { 
+    displayToTopBtn()
+};
+
+function displayToTopBtn() {
+    const toTheTopBtn = document.getElementById('top');
+    // Check scroll position
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        toTheTopBtn.style.display = 'block';
+    } else {
+        toTheTopBtn.style.display = 'none';
+    }
+}
