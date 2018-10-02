6da91243b9637ff95f17b5d43e7e8b4088412e87
Kristy Burge <hello@kristyburge.com>
2018-10-02 10:49:53 -0700

    Load contact info using data file

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+// Load contact information
+$.getJSON( "../data/contact.json", function( data ) {
+
+  $('span .phone').append('<a href="tel:' + data[0].phone + '" class="phone"> ' + data[0].phone + '</a>');
+  $('span .email').append('<a href="mailto:' + data[0].email + '" class="email"> ' + data[0].email + '</a>');
+
+});
--- js/projects.js
+++ js/projects.js
@@ -13,7 +13,6 @@ function createCard(id, name, description, image, github, url){
   card.setAttribute('id', `card-${id}`);
 
   const lightbox = document.createElement('A');
-  lightbox.setAttribute('href', '#');
 
   const projectImage = document.createElement('IMG');
   projectImage.setAttribute('src', image);
@@ -36,17 +35,17 @@ function createCard(id, name, description, image, github, url){
   const githubLink = document.createElement('A');
   githubLink.setAttribute('href', github);
   githubLink.setAttribute('target', '_blank');
-  githubLink.classList.add('button', 'primary');
+  githubLink.classList.add('button', 'primary', 'hollow');
   githubLink.innerText = 'See project files';
 
   const liveProject = document.createElement('A');
   liveProject.setAttribute('href', url);
   liveProject.setAttribute('target', '_blank');
-  liveProject.classList.add('button', 'success');
+  liveProject.classList.add('button', 'success', 'hollow');
   liveProject.innerText = 'See it live!';
 
   const openModal = document.createElement('BUTTON');
-  openModal.classList.add('button');
+  openModal.classList.add('button', 'hollow', 'secondary');
   openModal.setAttribute('data-open', `project-${id}`)
   openModal.innerText = 'Open details';
 
@@ -137,36 +136,11 @@ function createOverlay(id, name, description, image, github, url){
 
 }
 
-// ================
-// EVENTS - do this inside the readystate
-// ================
-
-      //
-      //     $('.prev').on('click', getPreviousCard);
-      //
-      //     // OR - listen for a left arrow key press
-      //     $(document).keydown(function(e) {
-      //       if (e.which === 37) {
-      //         getPreviousCard();
-      //       }
-      //     });
-      //
-      //     $('.next').on('click', getNextCard);
-      //
-      //     // OR - listen for a right arrow key press
-      //     $(document).keydown(function(e) {
-      //       if (e.which === 39) {
-      //         getNextCard();
-      //       }
-      //     });
-      //
-      //
-      //
-      // });
-
 // =============================
 // Connect to Projects JSON file
 // =============================
+const loading = document.querySelector('.loading');
+
 
 // 1. Create new XHR object
 const xhr = new XMLHttpRequest();
@@ -177,6 +151,9 @@ xhr.onreadystatechange = function(){
     if (xhr.status === 200) {
       // Success - convert results to an object
       const results = JSON.parse(xhr.responseText);
+
+      // Remove loading indicator
+      loading.style.display = 'none';
 
       // Loop through the results and create a card for each project
       results.forEach( result => {
@@ -227,9 +204,16 @@ xhr.onreadystatechange = function(){
               }
 
             });
-
-    } // end status === 200
-  } // end readyState 4
+      // end status === 200
+    } else {
+      loading.style.display = 'block';
+      loading.innerHTML = 'Sorry, something went wrong. Error: ' + xhr.statusText;
+    }
+  // end readyState 4
+  } else {
+    loading.style.display = 'block';
+    loading.innerHTML = 'Sorry, something went wrong. Error: ' + xhr.statusText;
+  }
 };
 
 // 3. Open the request
