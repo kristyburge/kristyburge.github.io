cb85d06155819eed378ac48057182514f3c11c09
Kristy Burge <kristyburge@gmail.com>
2019-04-09 18:19:43 -0500

    Update header content, contact section, and prettify files

--- js/app.js
+++ js/app.js
@@ -23,19 +23,34 @@ function displayToTopBtn() {
 // ============
 // Typed library by https://mattboldt.com/demos/typed-js/
 const typed = new Typed('#typed', {
-  strings: ["Do you want... ^500 more revenue?", "Do you want... ^500 to save time?", "Do you want... ^500 happy customers?"],
-  typeSpeed: 100,
-  smartBackspace: true,
-  startDelay: 200
+    strings: ["JavaScript...", "HTML5...", "CSS3...", "Front-End Developer."],
+    typeSpeed: 100,
+    smartBackspace: true,
+    startDelay: 200
 });
 
 // ============
 // LOAD DATA
 // ============
 // Load contact information
-$.getJSON( "../data/contact.json", function( data ) {
-  $('span .phone').append('<a href="tel:+1-' + data[0].phone + '" class="phone"> ' + data[0].phone + '</a>');
-  $('span .email').append('<a href="mailto:' + data[0].email + '" class="email"> ' + data[0].email + '</a>');
+$.getJSON("../data/contact.json", function(data) {
+    $('span .phone').append('<a href="tel:+1-' + data[0].phone + '" class="phone"> ' + data[0].phone + '</a>');
+    $('span .email').append('<a href="mailto:' + data[0].email + '" class="email"> ' + data[0].email + '</a>');
+});
+
+// CONTACT 
+const alertBox = document.querySelector('.alert-box');
+
+document.querySelector('.alert-box').addEventListener('click', function(e) {
+    if (e.target.nodeName === 'BUTTON') {
+        alertBox.style.opacity = 0;
+    }
+
+});
+
+document.querySelector('.contact--form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alertBox.style.opacity = 1;
 });
 
 
@@ -48,79 +63,79 @@ const modalWrapper = document.querySelector('.modal');
 const api = '../data/projects.json';
 
 // Create a card for each project
-function createCard(id, name, description, image, github, url){
-  const cell = document.createElement('DIV');
-  cell.classList.add('medium-6', 'large-4', 'cell');
+function createCard(id, name, description, image, github, url) {
+    const cell = document.createElement('DIV');
+    cell.classList.add('medium-6', 'large-4', 'cell');
 
-  const card = document.createElement('DIV');
-  card.classList.add('card');
-  card.setAttribute('id', `card-${id}`);
+    const card = document.createElement('DIV');
+    card.classList.add('card');
+    card.setAttribute('id', `card-${id}`);
 
-  const lightbox = document.createElement('A');
+    const lightbox = document.createElement('A');
 
-  const projectImage = document.createElement('IMG');
-  projectImage.setAttribute('src', image);
-  projectImage.setAttribute('alt', name);
+    const projectImage = document.createElement('IMG');
+    projectImage.setAttribute('src', image);
+    projectImage.setAttribute('alt', name);
 
-  const cardSection = document.createElement('DIV');
-  cardSection.classList.add('card-section');
+    const cardSection = document.createElement('DIV');
+    cardSection.classList.add('card-section');
 
-  const projectName = document.createElement('H3');
-  projectName.classList.add('text-center');
-  projectName.innerText = name;
+    const projectName = document.createElement('H3');
+    projectName.classList.add('text-center');
+    projectName.innerText = name;
 
-  const divider = document.createElement('HR');
+    const divider = document.createElement('HR');
 
-  const projectDesc = document.createElement('P');
-  projectDesc.innerText = description.slice(0,103).concat('...');
+    const projectDesc = document.createElement('P');
+    projectDesc.innerText = description.slice(0, 103).concat('...');
 
-  const linksContainer = document.createElement('DIV');
-  linksContainer.classList.add('links');
+    const linksContainer = document.createElement('DIV');
+    linksContainer.classList.add('links');
 
-  const githubLink = document.createElement('A');
-  githubLink.setAttribute('href', github);
-  githubLink.setAttribute('target', '_blank');
-  githubLink.classList.add('button', 'primary', 'hollow');
-  githubLink.innerText = 'See project files';
+    const githubLink = document.createElement('A');
+    githubLink.setAttribute('href', github);
+    githubLink.setAttribute('target', '_blank');
+    githubLink.classList.add('button', 'primary', 'hollow');
+    githubLink.innerText = 'See project files';
 
-  const liveProject = document.createElement('A');
-  liveProject.setAttribute('href', url);
-  liveProject.setAttribute('target', '_blank');
-  liveProject.classList.add('button', 'success', 'hollow');
-  liveProject.innerText = 'See it live!';
+    const liveProject = document.createElement('A');
+    liveProject.setAttribute('href', url);
+    liveProject.setAttribute('target', '_blank');
+    liveProject.classList.add('button', 'success', 'hollow');
+    liveProject.innerText = 'See it live!';
 
-  const openModal = document.createElement('BUTTON');
-  openModal.classList.add('button', 'hollow', 'secondary');
-  openModal.setAttribute('data-open', `project-${id}`);
-  openModal.innerText = 'Open details';
+    const openModal = document.createElement('BUTTON');
+    openModal.classList.add('button', 'hollow', 'secondary');
+    openModal.setAttribute('data-open', `project-${id}`);
+    openModal.innerText = 'Open details';
 
-  linksContainer.appendChild(githubLink);
-  linksContainer.appendChild(liveProject);
-  linksContainer.appendChild(openModal);
+    linksContainer.appendChild(githubLink);
+    linksContainer.appendChild(liveProject);
+    linksContainer.appendChild(openModal);
 
-  cardSection.appendChild(projectName);
-  cardSection.appendChild(divider);
-  cardSection.appendChild(projectDesc);
-  cardSection.appendChild(linksContainer);
+    cardSection.appendChild(projectName);
+    cardSection.appendChild(divider);
+    cardSection.appendChild(projectDesc);
+    cardSection.appendChild(linksContainer);
 
-  lightbox.appendChild(projectImage);
+    lightbox.appendChild(projectImage);
 
-  card.appendChild(lightbox);
-  card.appendChild(cardSection);
+    card.appendChild(lightbox);
+    card.appendChild(cardSection);
 
-  cell.appendChild(card);
+    cell.appendChild(card);
 
-  projectsContainer.appendChild(cell);
+    projectsContainer.appendChild(cell);
 }
 
 // Create an overlay lightbox for each project
-function createOverlay(id, name, description, skills, image, github, url){
-  const modalContainer = document.createElement('DIV');
-  modalContainer.classList.add('popup');
-  modalContainer.setAttribute('id', `project-${id}`);
+function createOverlay(id, name, description, skills, image, github, url) {
+    const modalContainer = document.createElement('DIV');
+    modalContainer.classList.add('popup');
+    modalContainer.setAttribute('id', `project-${id}`);
 
-  const projectDetails = document.createElement('DIV');
-  projectDetails.classList.add('project-details');
+    const projectDetails = document.createElement('DIV');
+    projectDetails.classList.add('project-details');
 
     const img = document.createElement('IMG');
     img.setAttribute('src', image);
@@ -128,65 +143,65 @@ function createOverlay(id, name, description, skills, image, github, url){
     const moreDetails = document.createElement('DIV');
     moreDetails.classList.add('project-details--more');
 
-      const projectName = document.createElement('H3');
-      projectName.innerText = name;
+    const projectName = document.createElement('H3');
+    projectName.innerText = name;
 
-      const divider = document.createElement('HR');
+    const divider = document.createElement('HR');
 
-      const projectDesc = document.createElement('P');
-      projectDesc.innerText = description;
+    const projectDesc = document.createElement('P');
+    projectDesc.innerText = description;
 
-      const skillsContainer = document.createElement('UL');
+    const skillsContainer = document.createElement('UL');
 
-      skills.forEach( skill => {
+    skills.forEach(skill => {
         let skillItem = document.createElement('LI');
         skillItem.innerText = skill;
         skillsContainer.appendChild(skillItem);
-      });
+    });
 
-      const linksContainer = document.createElement('DIV');
-      linksContainer.classList.add('links');
+    const linksContainer = document.createElement('DIV');
+    linksContainer.classList.add('links');
 
-        const githubLink = document.createElement('A');
-        githubLink.setAttribute('href', github);
-        githubLink.setAttribute('target', '_blank');
-        githubLink.classList.add('button', 'primary');
-        githubLink.innerText = 'See project files';
+    const githubLink = document.createElement('A');
+    githubLink.setAttribute('href', github);
+    githubLink.setAttribute('target', '_blank');
+    githubLink.classList.add('button', 'primary');
+    githubLink.innerText = 'See project files';
 
-        const liveProject = document.createElement('A');
-        liveProject.setAttribute('href', url);
-        liveProject.setAttribute('target', '_blank');
-        liveProject.classList.add('button', 'success');
-        liveProject.innerText = 'See it live!';
+    const liveProject = document.createElement('A');
+    liveProject.setAttribute('href', url);
+    liveProject.setAttribute('target', '_blank');
+    liveProject.classList.add('button', 'success');
+    liveProject.innerText = 'See it live!';
 
-  const closeModalBtn = document.createElement('BUTTON');
-  closeModalBtn.classList.add('close-button');
-  closeModalBtn.setAttribute('data-close', '');
-  closeModalBtn.setAttribute('aria-label', 'Close modal');
-  closeModalBtn.setAttribute('type', 'button');
+    const closeModalBtn = document.createElement('BUTTON');
+    closeModalBtn.classList.add('close-button');
+    closeModalBtn.setAttribute('data-close', '');
+    closeModalBtn.setAttribute('aria-label', 'Close modal');
+    closeModalBtn.setAttribute('type', 'button');
 
     const span = document.createElement('SPAN');
     span.setAttribute('aria-hidden', 'true');
     span.innerHTML = 'X';
 
-  linksContainer.appendChild(githubLink);
-  linksContainer.appendChild(liveProject);
+    linksContainer.appendChild(githubLink);
+    linksContainer.appendChild(liveProject);
 
-  moreDetails.appendChild(projectName);
-  moreDetails.appendChild(divider);
-  moreDetails.appendChild(projectDesc);
-  moreDetails.appendChild(skillsContainer);
-  moreDetails.appendChild(linksContainer);
+    moreDetails.appendChild(projectName);
+    moreDetails.appendChild(divider);
+    moreDetails.appendChild(projectDesc);
+    moreDetails.appendChild(skillsContainer);
+    moreDetails.appendChild(linksContainer);
 
-  closeModalBtn.appendChild(span);
+    closeModalBtn.appendChild(span);
 
-  projectDetails.appendChild(img);
-  projectDetails.appendChild(moreDetails);
+    projectDetails.appendChild(img);
+    projectDetails.appendChild(moreDetails);
 
-  modalContainer.appendChild(projectDetails);
-  modalContainer.appendChild(closeModalBtn);
+    modalContainer.appendChild(projectDetails);
+    modalContainer.appendChild(closeModalBtn);
 
-  modalWrapper.appendChild(modalContainer);
+    modalWrapper.appendChild(modalContainer);
 
 }
 
@@ -200,74 +215,74 @@ const loading = document.querySelector('.loading');
 const xhr = new XMLHttpRequest();
 
 // 2. Callback function to run
-xhr.onreadystatechange = function(){
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      // Success - convert results to an object
-      const results = JSON.parse(xhr.responseText);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            // Success - convert results to an object
+            const results = JSON.parse(xhr.responseText);
 
-      // Remove loading indicator
-      loading.style.display = 'none';
+            // Remove loading indicator
+            loading.style.display = 'none';
 
-      // Loop through the results and create a card for each project
-      results.forEach( result => {
-        createCard(result.id, result.name, result.description, result.image, result.github, result.url);
-        createOverlay(result.id, result.name, result.description, result.skills, result.image, result.github, result.url);
-      });
+            // Loop through the results and create a card for each project
+            results.forEach(result => {
+                createCard(result.id, result.name, result.description, result.image, result.github, result.url);
+                createOverlay(result.id, result.name, result.description, result.skills, result.image, result.github, result.url);
+            });
 
-      // ==============
-      // EVENTS:
-      // When a project is clicked, open the modal for that project
-      // Close the modal by clicking the close button or hitting escape on keyboard
-      // ===============
+            // ==============
+            // EVENTS:
+            // When a project is clicked, open the modal for that project
+            // Close the modal by clicking the close button or hitting escape on keyboard
+            // ===============
             // Listen on the PARENT card for the click event
-            $('.projects-container .card').on('click', function(e){
-              // only open the card if the user clicks the open details button
-              if (e.target.hasAttribute('data-open') || e.target.tagName === 'IMG') {
-                  console.log(this);
-                // Get the value of the employee #card-X
-                var cardID = $(this).attr('id');
-                var projectNumber = cardID.slice(5,7);
+            $('.projects-container .card').on('click', function(e) {
+                // only open the card if the user clicks the open details button
+                if (e.target.hasAttribute('data-open') || e.target.tagName === 'IMG') {
+                    console.log(this);
+                    // Get the value of the employee #card-X
+                    var cardID = $(this).attr('id');
+                    var projectNumber = cardID.slice(5, 7);
 
-                // Display the modal overlay
-                $('.modal').css('left', 0);
+                    // Display the modal overlay
+                    $('.modal').css('left', 0);
 
-                // Find the project popup to shadow
-                var projectToShow = document.getElementById('project-' + projectNumber);
-                projectToShow.style.display = 'flex';
-                projectToShow.classList.add('selected');
+                    // Find the project popup to shadow
+                    var projectToShow = document.getElementById('project-' + projectNumber);
+                    projectToShow.style.display = 'flex';
+                    projectToShow.classList.add('selected');
 
-                // Close the Modal if user clicks the X
-                $('.close-button').on('click', function(){
-                  // Hide the overlay & card
-                  $('.modal').css('left', '100%');
-                  $('.modal .popup').css('display', 'none');
-                  $('.modal .popup').removeClass('selected');
-                });
+                    // Close the Modal if user clicks the X
+                    $('.close-button').on('click', function() {
+                        // Hide the overlay & card
+                        $('.modal').css('left', '100%');
+                        $('.modal .popup').css('display', 'none');
+                        $('.modal .popup').removeClass('selected');
+                    });
 
-                // OR - listen for a keydown event and close the modal when ESC key is pressed
-                $(document).keydown(function(e) {
-                  if (e.which === 27) {
-                    // Hide the overlay & card
-                    $('.modal').css('left', '100%');
-                    $('.modal .popup').css('display', 'none');
-                    $('.modal .popup').removeClass('selected');
-                  }
-                });
+                    // OR - listen for a keydown event and close the modal when ESC key is pressed
+                    $(document).keydown(function(e) {
+                        if (e.which === 27) {
+                            // Hide the overlay & card
+                            $('.modal').css('left', '100%');
+                            $('.modal .popup').css('display', 'none');
+                            $('.modal .popup').removeClass('selected');
+                        }
+                    });
 
-              }
+                }
 
             });
-      // end status === 200
+            // end status === 200
+        } else {
+            loading.style.display = 'block';
+            loading.innerHTML = 'Sorry, something went wrong. Error: ' + xhr.status;
+        }
+        // end readyState 4
     } else {
-      loading.style.display = 'block';
-      loading.innerHTML = 'Sorry, something went wrong. Error: ' + xhr.status;
+        loading.style.display = 'block';
+        loading.innerHTML = 'Unable to connect. Please try again later.' + xhr.status;
     }
-  // end readyState 4
-  } else {
-    loading.style.display = 'block';
-    loading.innerHTML = 'Unable to connect. Please try again later.' + xhr.status;
-  }
 };
 
 // 3. Open the request
